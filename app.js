83643336520f67fb2bf492bd66c4c34fea7f6eca
Agustin Macazzaga <ag.macazz@gmail.com>
2021-10-06 15:37:09 -0300

    variables y operadores, done

---
+++ app.js
@@ -0,0 +1,25 @@
+/* ////////////// Variables y Operadores//////////////////////
+a-Crear dos variables numéricas y utilizar el operador suma para
+ guardar el valor de la suma de 
+ambos números en una 3er variable.
+b-Crear dos variables de tipo String y concatenarlas guardando el resultado en 
+una 3er variable.
+c-Crear dos variables de tipo String y sumar el largo de cada variable 
+(cantidad de letras del string) guardando el resultado de la suma en una 
+3er variable (utilizar length).
+/////////////////////////Strings/////////////////////////*/
+ 
+let a = 10
+let b = 20
+let c = a+b
+console.log(c)
+/////////////////////////////////////////////
+let primeraString = "hola"
+let segundaString = "mundo"
+let sumaString = primeraString+segundaString
+console.log (sumaString)
+///////////////////////////////
+let terceraString = "buen"
+let cuartaString = "dia"
+let stringLength = terceraString.length + cuartaString.length
+console.log (stringLength)
